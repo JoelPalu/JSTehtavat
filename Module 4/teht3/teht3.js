5bc186bf0d50fdd876477f1e9b847e7bfa559bd5
JoelPalu <122279670+JoelPalu@users.noreply.github.com>
2023-04-17 20:43:32 +0300

    Module 4

--- Module 4/teht3/teht3.js
+++ Module 4/teht3/teht3.js
@@ -1,12 +1,18 @@
 
 document.querySelector('#form').addEventListener('submit', async function(evt) {
     evt.preventDefault();
+
     const code = document.querySelector('#query').value;
+
     try {
         const response = await fetch (`https://api.tvmaze.com/search/shows?q=${code}`);
         const jsonData = await response.json();
         console.log(jsonData);
         const main = document.querySelector('#results');
+        while (main.firstChild) {
+            main.removeChild(main.lastChild);
+          }
+
         for(let i = 0; i < jsonData.length; i++){
             const article = document.createElement("article");
 
